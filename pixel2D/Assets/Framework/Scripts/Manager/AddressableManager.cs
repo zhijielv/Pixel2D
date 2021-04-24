831971d4d626cb6916787da7c3473a72ca135834
乄风铃彡 <375387924@qq.com>
2021-04-24 16:08:01 +0800

    资源加载更新

--- pixel2D/Assets/Framework/Scripts/Manager/AddressableManager.cs
+++ pixel2D/Assets/Framework/Scripts/Manager/AddressableManager.cs
@@ -1,20 +1,70 @@
 ﻿using Framework.Scripts.Singleton;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using System;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Framework.Scripts.Manager
 {
     public class AddressableManager : ManagerSingleton<AddressableManager>
     {
-        public AddressableManager()
+        private static bool _initialized = false;
+        public static bool Ready => _initialized;
+
+        static void InitDone(AsyncOperationHandle<IResourceLocator> obj)
         {
-            Addressables.InitializeAsync();
+            _initialized = true;
+        }
+
+        public void Start()
+        {
+            Addressables.InitializeAsync().Completed += InitDone;
             Addressables.DownloadDependenciesAsync("preload");
         }
 
-        public void LoadView(string viewName, Transform parent, bool worldSpace = false, bool trackHandle = true)
+        public T LoadAsset<T>(object key)
         {
-            Addressables.InstantiateAsync(viewName, parent, worldSpace, trackHandle);
+            if (!_initialized)
+                throw new Exception("Whoa there friend!  We haven't init'd yet!");
+
+            var op = Addressables.LoadAssetAsync<T>(key);
+
+            if (!op.IsDone)
+                throw new Exception("Sync LoadAsset failed to load in a sync way! " + key);
+
+            if (op.Result == null)
+            {
+                var message = "Sync LoadAsset has null result " + key;
+                if (op.OperationException != null)
+                    message += " Exception: " + op.OperationException;
+
+                throw new Exception(message);
+            }
+
+            return op.Result;
+        }
+
+        public GameObject Instantiate(object key, Transform parent = null, bool instantiateInWorldSpace = false)
+        {
+            if (!_initialized)
+                throw new Exception("Whoa there friend!  We haven't init'd yet!");
+
+            var op = Addressables.InstantiateAsync(key, parent, instantiateInWorldSpace);
+
+            if (!op.IsDone)
+                throw new Exception("Sync Instantiate failed to finish! " + key);
+
+            if (op.Result == null)
+            {
+                var message = "Sync Instantiate has null result " + key;
+                if (op.OperationException != null)
+                    message += " Exception: " + op.OperationException;
+
+                throw new Exception(message);
+            }
+
+            return op.Result;
         }
     }
 }
