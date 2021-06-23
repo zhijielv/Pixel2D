a94e68902ebc7329335a2203487cb6e09cbd6f8c
乄风铃彡 <375387924@qq.com>
2021-06-23 10:17:39 +0800

    添加空ui组件，实现Graphic，减少DrawCall

---
+++ pixel2D/Assets/Framework/Scripts/UI/CustomUI/ClickOnly.cs
@@ -0,0 +1,20 @@
+﻿/*
+** Created by fengling
+** DateTime:    2021-06-23 10:03:07
+** Description: 接受点击事件的空ui 
+*/
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Framework.Scripts.UI.CustomUI
+{
+    public class ClickOnly : CustomGraphic, IPointerClickHandler
+    {
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log(eventData.position + "  " + eventData.clickCount);
+        }
+    }
+}
---
+++ pixel2D/Assets/Framework/Scripts/UI/CustomUI/CustomGraphic.cs
@@ -0,0 +1,21 @@
+﻿/*
+** Created by fengling
+** DateTime:    2021-06-23 09:54:01
+** Description: 接受Raycast的空UI组件 
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Framework.Scripts.UI.CustomUI
+{
+    [RequireComponent(typeof(CanvasRenderer))]
+    public abstract class CustomGraphic : Graphic
+    {
+        protected override void Start()
+        {
+            base.Start();
+            this.color = Color.clear;
+        }
+    }
+}
